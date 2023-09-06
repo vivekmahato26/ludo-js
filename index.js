e2d41757f1b432f87afcc12f43a379caafec429e
vivekmahato26 <vivekmahato26@gmail.com>
2023-09-06 15:02:13 +0530

    player class

--- index.js
+++ index.js
@@ -224,13 +224,25 @@ const rb = validMovesArr
   .filter((e) => e.j >= 9 && e.j <= 14 && e.i == 8)
   .sort((a, b) => b.j - a.j);
 
-const index70 = validMovesArr.findIndex(e => e.i == 7 && e.j == 0); 
-const index07 = validMovesArr.findIndex(e => e.i == 0 && e.j == 7); 
-const index714 = validMovesArr.findIndex(e => e.i == 7 && e.j == 14); 
-const index147 = validMovesArr.findIndex(e => e.i == 14 && e.j == 7);
+const index70 = validMovesArr.findIndex((e) => e.i == 7 && e.j == 0);
+const index07 = validMovesArr.findIndex((e) => e.i == 0 && e.j == 7);
+const index714 = validMovesArr.findIndex((e) => e.i == 7 && e.j == 14);
+const index147 = validMovesArr.findIndex((e) => e.i == 14 && e.j == 7);
 
-const movesArr = [...bl,...lb,validMovesArr[index70],...lt,...tl,validMovesArr[index07],...tr,...rt,validMovesArr[index714],...rb,...br,validMovesArr[index147]];
-
+const movesArr = [
+  ...bl,
+  ...lb,
+  validMovesArr[index70],
+  ...lt,
+  ...tl,
+  validMovesArr[index07],
+  ...tr,
+  ...rt,
+  validMovesArr[index714],
+  ...rb,
+  ...br,
+  validMovesArr[index147],
+];
 
 class Coin {
   constructor(code, color, currenti, currentj) {
@@ -238,16 +250,155 @@ class Coin {
     this.color = color;
     this.currenti = currenti;
     this.currentj = currentj;
+    this.safe = false;
   }
   move = (diceOutput) => {
     const currentIndex = movesArr.findIndex(
       (e) => e.i == this.currenti && e.j == this.currentj
     );
     let newIndex = currentIndex + diceOutput;
-    if (newIndex >= movesArr.length)
-      newIndex = newIndex - movesArr.length - 1;
+    if (newIndex >= movesArr.length) newIndex = newIndex - movesArr.length - 1;
     const box = movesArr[newIndex];
     this.currenti = box.i;
     this.currentj = box.j;
   };
+}
+
+class Player {
+  constructor(color, name, coins, turn, safeStart, safeArr) {
+    this.color = color;
+    this.name = name;
+    this.locked = 4;
+    this.passesd = 0;
+    this.coins = coins;
+    this.turn = turn;
+    this.safeStart = safeStart;
+    this.safeArr = safeArr;
+  }
+}
+
+const players = [];
+
+const blueSafeIndex = movesArr.findIndex((e) => e.i == 14 && e.j == 7);
+const blueSafeArr = validMovesArr
+  .filter((e) => e.type == "blue-safe")
+  .sort((a, b) => a.i - b.i);
+const redSafeIndex = movesArr.findIndex((e) => e.i == 7 && e.j == 0);
+const redSafeArr = validMovesArr
+  .filter((e) => e.type == "red-safe")
+  .sort((a, b) => a.j - b.j);
+const greenSafeIndex = movesArr.findIndex((e) => e.i == 0 && e.j == 7);
+const greenSafeArr = validMovesArr
+  .filter((e) => e.type == "green-safe")
+  .sort((a, b) => b.i - a.i);
+const yellowSafeIndex = movesArr.findIndex((e) => e.i == 7 && e.j == 14);
+const yellowSafeArr = validMovesArr
+  .filter((e) => e.type == "yellow-safe")
+  .sort((a, b) => b.j - a.j);
+
+const playerNum = prompt("Select total players max 4");
+
+for (let i = 0; i < parseInt(playerNum) && i < 4; i++) {
+  const coins = [];
+  for (let j = 0; j < 4; j++) {
+    coins.push(new Coin(`b${j}`, "blue", 0, 0));
+  }
+  const bluePlayer = new Player(
+    "blue",
+    "bluePlayer",
+    [...coins],
+    true,
+    blueSafeIndex,
+    blueSafeArr
+  );
+  coins.length = 0;
+  for (let j = 0; j < 4; j++) {
+    coins.push(new Coin(`g${j}`, "green", 0, 0));
+  }
+  const greenPlayer = new Player(
+    "green",
+    "greenPlayer",
+    [...coins],
+    false,
+    greenSafeIndex,
+    greenSafeArr
+  );
+  coins.length = 0;
+  for (let j = 0; j < 4; j++) {
+    coins.push(new Coin(`r${j}`, "red", 0, 0));
+  }
+  const redPlayer = new Player(
+    "red",
+    "redPlayer",
+    [...coins],
+    false,
+    redSafeIndex,
+    redSafeArr
+  );
+  coins.length = 0;
+  for (let j = 0; j < 4; j++) {
+    coins.push(new Coin(`y${j}`, "yellow", 0, 0));
+  }
+  const yellowPlayer = new Player(
+    "yellow",
+    "yellowPlayer",
+    [...coins],
+    false,
+    yellowSafeIndex,
+    yellowSafeArr
+  );
+  coins.length = 0;
+  if (parseInt(playerNum) == 2) {
+    let temp = true;
+    while (temp) {
+      const color = prompt("Select Color");
+      switch (color) {
+        case "blue":
+          players.push(bluePlayer);
+          players.push(greenPlayer);
+          temp = false;
+          break;
+        case "red":
+          players.push(redPlayer);
+          players.push(yellowPlayer);
+          temp = false;
+          break;
+        case "green":
+          players.push(greenPlayer);
+          players.push(bluePlayer);
+          temp = false;
+          break;
+        case "yellow":
+          players.push(yellowPlayer);
+          players.push(redPlayer);
+          temp = false;
+          break;
+        default: alert("select a valid color")
+      }
+    }
+    break;
+  }
+  const color = prompt("select color");
+  switch (color) {
+    case "red": {
+      players.push(redPlayer);
+      break;
+    }
+    case "blue": {
+      players.push(bluePlayer);
+      break;
+    }
+    case "green": {
+      players.push(greenPlayer);
+      break;
+    }
+    case "yellow": {
+      players.push(yellowPlayer);
+      break;
+    }
+    default: {
+      i--;
+      alert("select a valid color");
+    }
+  }
 }
